51cb71524e5c32e92ea66400a77323befd08913c
kreper <kreper375@gmail.com>
2022-12-17 21:10:43 +0300

    lesson1 фикс 2 задачи

--- Семинар1/задача2.py
+++ Семинар1/задача2.py
@@ -1,9 +1,9 @@
 #2. Напишите программу для проверки ложности утверждения (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
-x = int(input('Введите число x '))
-y = int(input('Введите число y '))
-z = int(input('Введите число z '))
 
-for x in [True,False]:
-    for y in [True,False]:
-        for z in [True,False]:
-            print(x,'AND',y,'OR',z,'=',x and y or z)
+print("x y z w")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                print(not (x or y or z or w) == (not x and not y and not z and not w))
+                print(x, y, z, w)

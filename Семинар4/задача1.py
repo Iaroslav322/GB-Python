d211fc7407d0e6901bcb9583b42c93abccb1e164
kreper <kreper375@gmail.com>
2022-12-28 00:32:07 +0300

    lesson4fix

--- Семинар4/задача1.py
+++ Семинар4/задача1.py
@@ -1,13 +1,8 @@
 #1. Вычислить число c заданной точностью d
-from decimal import *
-n = int(input('Введите число N: '))
-getcontext()
-Context(prec=28, rounding=ROUND_HALF_EVEN,
-        Emin=-999999, Emax=999999, capitals=1,
-        clamp=0, flags=[], traps=[InvalidOperation,
-        DivisionByZero, Overflow])
-getcontext().prec = 10
-Decimal(n)
-print ()
+from decimal import Decimal
 
+a = float(input("Введите число: "))
+number = Decimal(a)
+str = input("Введите точность: ")
+print(number.quantize(Decimal(str)))
 

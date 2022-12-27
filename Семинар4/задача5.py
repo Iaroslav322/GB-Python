be90d26412a45bee132b83b1fe90ce946422dd05
kreper <kreper375@gmail.com>
2022-12-27 23:01:53 +0300

    lesson4fix

--- Семинар4/задача5.py
+++ Семинар4/задача5.py
@@ -1,58 +1,50 @@
 #5. Даны два файла, в каждом из которых находится запись многочленов. Задача - сформировать файл, содержащий сумму многочленов.
-with open('mn_1.txt', 'w', encoding='utf-8') as file:
-    file.write('2*x^2 + 5*x^5')
-with open('mn_2.txt', 'w', encoding='utf-8') as file:
-    file.write('23*x^4 + 9*x^6')
+def remake(equation):
+    dictEquation = {}
+    equation = equation.replace(' - ', ' - ').replace(' + ',' + ')
+    equation = equation.split()
+    equation = equation[:-2]
+    for i in range(len(equation)):
+        equation[i] = equation[i].replace('+', '').split('x**')
+        dictEquation[int(equation[i][1])] = int(equation[i][0])
+    return dictEquation   
 
-with open('mn_1.txt','r') as file:
-    mn_1 = file.readline()
-    list_of_mn_1 = mn_1.split()
+def sumEqution(dict1, dict2):
+    dictFinal = {}
+    maximum = (max(max(dict1), max(dict2)))
+    for i in range(maximum, -1, -1):
+        first = dict.get(i)
+        second = dict2.get(i)
+        if first != None or second != None:
+            dictFinal[i] = (first if first != None else 0) + (second if second != None else 0)
+    return dictFinal 
+def coefResult(dictFinal):
+    result = ''
+    for i in dictFinal.items():
+        if result == '':
+            if i [1] < 0:
+                result += ' - ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))
+            elif i[1] > 0:
+                result += str(abs(i[1])) + 'x^' + str(abs(i[0]))
+        else:
+            if i [1] < 0:
+                result += ' - ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))
+            elif i[1] > 0:
+                result += ' + ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))    
+        result = result.replace('x^1', 'x').replace('x^0', '').replace('1x^', 'x^')         
+    return result + ' = 0' 
+with open('file5.1.txt', 'r', encoding='utf-8') as text:
+    equation = text.readline()
+with open('file5.2.txt', 'r', encoding='utf-8') as text:
+    equation2 = text.readline()   
+print(equation)
+print(equation2)
+dictEquation = remake(equation)
+dictEquation2 = remake(equation2)
+dictFinal = sumEqution(dictEquation, dictEquation2)
+print(dictFinal)
 
-
-with open('mn_2.txt','r') as file:
-    mn_2 = file.readline()
-    list_of_mn_2 = mn_2.split()
-
-print(f'{list_of_mn_1} + {list_of_mn_2}')
-sum_mn = list_of_mn_1 + list_of_mn_2
-
-with open('sum_mn.txt', 'w', encoding='utf-8') as file:
-    file.write(f'{list_of_mn_1} + {list_of_mn_2}')
-
-
-#sum_of_mn = (list_of_mn_1), '+' (list_of_mn_2)
-
-
-
-
-if len(mn_1) > len(mn_2):
-    help_mn = mn_1
-    mn_1 = mn_2
-    mn_2=help_mn
-mn_1 = mn_1.split(' + ')
-mn_2 = mn_2.split(' + ')
-print(mn_1,mn_2)
-
-count1 =0
-count2=len(mn_2)-len(mn_1)
-new_mn = ''
-for i in range(count2):
-    new_mn += mn_2[i] + '+'
-
-ind1 = ''
-ind2 = ''
-
-for i in range(len(mn_2) - len(mn_1), len(mn_2)):
-    result = 0 
-    if i == len(mn_2) - 1:
-        result += int(mn_1[-1][:-4]+mn_2[-1][:-4])
-        new_mn += str(result) + mn_1[-1][-4:]
-    elif i == len(mn_2) - 2:
-        result += int(mn_1[-2][:-2]+mn_2[-2][:-2])
-        new_mn += str(result) + mn_1[-2][-2:] + ' + ' 
-    else:
-        result += int(mn_1[count1][:-4]+mn_2[count2][:-4])
-        new_mn += str(result) + mn_1[count1][-4:] + ' + ' 
-        count1 += 1
-        count2 += 2
-print(new_mn)
+dictFinal = coefResult(dictFinal)
+print(dictFinal)
+with open('file5.3.txt', 'w', encoding='utf-8') as text:
+    text.writelines(dictFinal)

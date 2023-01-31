400cc3111b87309c97b8831213ba8ba2e46d9c1f
kreper <kreper375@gmail.com>
2023-01-31 22:27:13 +0300

    calc_telegram

--- Калькулятор/main.py
+++ Калькулятор/main.py
@@ -3,11 +3,10 @@ from logger import result_logger as write_log
 import controller as d_t
 import console as c_ui
 
-
 def button_click():
     j = d_t.data_formatting(c_ui.input_data())
     calc_result = c_calc(j)
-    c_ui.view_data(calc_result, 'Ответ:')
+    c_ui.view_data(calc_result, 'Ответ: ')
     write_log(j, calc_result)
 
 button_click()
